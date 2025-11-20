281e3190dcfa5cdaceeac323dbe38e95a3840a49
Kaito420 <kaito420.lkr@gmail.com>
2025-11-20 11:02:36 +0900

    KTMATH修正

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -191,9 +191,9 @@ void Scene::RenderInspector()
 						ImGui::PopID();
 					}
 					if (changed) {
-						gameObject->_transform._quaternion = KTQUATERNION::FromEulerAngles(gameObject->_transform._rotation.y, gameObject->_transform._rotation.z, gameObject->_transform._rotation.x);
+						gameObject->_transform._quaternion = KTQUATERNION::FromEulerAngles(gameObject->_transform._rotation.x, gameObject->_transform._rotation.y, gameObject->_transform._rotation.z);
 						if (gameObject->GetComponent<RigidBody>()) {
-							gameObject->GetComponent<RigidBody>()->_orientation = KTQUATERNION::FromEulerAngles(gameObject->_transform._rotation.y, gameObject->_transform._rotation.z, gameObject->_transform._rotation.x);
+							gameObject->GetComponent<RigidBody>()->_orientation = KTQUATERNION::FromEulerAngles(gameObject->_transform._rotation.x, gameObject->_transform._rotation.y, gameObject->_transform._rotation.z);
 							gameObject->GetComponent<RigidBody>()->_angularVelocity = KTVECTOR3(0.0f, 0.0f, 0.0f);
 						}
 					}
