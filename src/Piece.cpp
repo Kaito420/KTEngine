76ff2a5f64cc3a2dad8d0cecb7a10fd89b0e38d7
Kaito420 <kaito420.lkr@gmail.com>
2026-03-26 11:12:29 +0900

    シーン作成

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -13,8 +13,6 @@ void Piece::Awake() {//ランダムに種類を決定（デバッグ用に種類を減らしている）
 void Piece::OnCollisionEnter(Collider* other)
 {
 	if (other->GetOwner()->GetComponent<Piece>()) {
-		//GameObject* explosion = Manager::GetCurrentScene()->AddGameObject<Explosion>();
-		//explosion->_transform._position = _owner->_transform._position + KTVECTOR3(0.0f, 0.0f, -1.0f);
 		if (_type == other->GetOwner()->GetComponent<Piece>()->_type) {//同じ種類なら消える
 			_owner->Destroy();
 			other->GetOwner()->Destroy();
